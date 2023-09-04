0455ea02e229601c9d71389b9b763efd08d1b858
Samuel Pacheco <samuelpachecopst@gmail.com>
2023-09-03 22:58:56 -0300

    dashboard

--- app/Providers/AdminServiceProvider.php
+++ app/Providers/AdminServiceProvider.php
@@ -2,12 +2,14 @@
 
 namespace App\Providers;
 
+use S4mpp\AdminPanel\Navigation\Page;
 use Illuminate\Support\ServiceProvider;
+use App\Http\Controllers\DashboardAdminController;
 
 class AdminServiceProvider extends ServiceProvider
 {
-    public function boot()
+    public function register()
     {
-        
+        Page::create('Dashboard')->target([DashboardAdminController::class, 'index'])->slug('dashboard')->order(0)->name('dashboard');
     }
 }
---
+++ config/admin.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+	'guard' => 'web',
+
+	'prefix' => 'painel',
+
+	'route_redirect_after_login' => 'dashboard'
+];
